5e153eae5ea62dc513627c83059b835c27a7c6f6
Emeryc P <emeryc.primeau@gmail.com>
2022-12-13 13:29:13 -0500

    stack and moveSet done - December5th

--- december5th.cpp
+++ december5th.cpp
@@ -5,7 +5,24 @@
 #include <string>
 #include <vector>
 #include <climits>
+#include <sstream>
 
+void extractIntegerWords(std::string str, std::vector<int>& vec)
+{
+    std::stringstream ss;
+    ss << str;
+
+    std::string temp;
+    int found;
+    while (!ss.eof()) {
+        ss >> temp;
+
+        if (std::stringstream(temp) >> found)
+            vec.push_back(found);
+
+        temp = "";
+    }
+}
 
 void December5th()
 {
@@ -13,10 +30,37 @@ void December5th()
     std::ifstream f( "inputs/december5th" );
     std::string s;
 
+    std::vector<std::vector<char>> stacks(9);
+    
     // Read file
     while (getline( f, s ))
     {
-        
+        std::vector<int> instructions;
+        if (s.find("from") != std::string::npos) {
+            extractIntegerWords(s, instructions);
+            // to add
+        }
+        else if(!s.empty())
+        {
+            int index = 0;
+            int vectorIdx = 0;
+            for (auto character : s)
+            {
+                if(index == 1 && (character > 57 || character < 49))
+                {
+                    stacks[vectorIdx].push_back(character);
+                    vectorIdx++;
+                }
+
+                if(vectorIdx == 9)
+                    break;
+                
+                if(index == 3)
+                    index = 0;
+                else
+                    index++;
+            }
+        }
     }
 
     
